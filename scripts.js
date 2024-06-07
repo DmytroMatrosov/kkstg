dd23eeca8b26ba275c608bf345b3bb7fdf4d726e
Dmytro Matrosov <dmytro.matrosov21@gmail.com>
2024-06-07 06:05:42 +0200

    fix: animation fix

--- scripts.js
+++ scripts.js
@@ -34,7 +34,7 @@ function scrollActive() {
 
   sections.forEach((current) => {
     const sectionHeight = current.offsetHeight;
-    const sectionTop = current.offsetTop - 96;
+    const sectionTop = current.offsetTop - 64;
     sectionId = current.getAttribute("id");
 
     if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
